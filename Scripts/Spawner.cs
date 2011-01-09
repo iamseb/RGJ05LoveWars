a24af4afb65a22ac7bbad2795a6454ff34e807d9
seb <iamseb@gmail.com>
2011-01-09 00:11:51 +0000

    fixed enemy spawning on timer, tracking in array

--- Scripts/Spawner.cs
+++ Scripts/Spawner.cs
@@ -9,8 +9,12 @@ public class Spawner : MonoBehaviour
 	public bool spawning = false;
 	public float timeSinceSpawn = 0.0f;
 	public float frequency = 1.0f;
+	public float spawnDelay = 0.2f;
 	public Transform[] badGuyTypes;
 	
+	void Awake(){
+	}
+		
 	void Start(){
 		Spawning = true;
 	}
@@ -24,10 +28,29 @@ public class Spawner : MonoBehaviour
 			int minEnemies = phaseAmount / 4;
 			if(timeSinceSpawn > phaseTime || enemies.Length <= minEnemies){
 				Debug.Log("Spawning " + phaseAmount + " enemies.");
+				StartCoroutine(Spawn(phaseAmount, phaseNum));
 				timeSinceSpawn = 0.0f;
 			}
 			yield return new  WaitForSeconds(frequency);
 		}
+	}
+	
+	public IEnumerator Spawn(int amount, int phase){
+		int currentLen = enemies.Length;
+		int nextLen = enemies.Length + amount;
+		Transform[] newEnemies = new Transform[nextLen];
+		
+		for(int i=0; i<currentLen; i++){
+			newEnemies[i] = enemies[i];
+		}
+		
+		for(int i=0; i<amount; i++){
+			Transform t = (Transform)Instantiate(badGuyTypes[phase]);
+			newEnemies[currentLen+i] = t;
+			yield return new WaitForSeconds(spawnDelay);
+		}
+		
+		enemies = newEnemies;
 	}
 	
 	public bool Spawning {
